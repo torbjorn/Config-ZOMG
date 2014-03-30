3639bd84eaab591595eb922bd3002c645aa4965c
Torbjørn Lindahl <torbjorn.lindahl@gmail.com>
2014-03-31 01:58:52 +0200

    work

--- lib/Config/ZOMG/CLSource.pm
+++ lib/Config/ZOMG/CLSource.pm
@@ -5,6 +5,14 @@ use lib '../Config-Loader/lib';
 use Moo;
 extends "Config::Loader::Source::Profile::Default";
 
+has name => (
+   is => 'rw',
+);
+
 with "Config::Loader::SourceRole::FileHelper";
+with "Config::Loader::SourceRole::FileFromEnv";
+
+## DEV AT HERE; CONTINUE MERGING MAIN CLASS INTO THIS, PREFERABLY AS
+## NOT TOO COMPLEX ROLES
 
 1;
